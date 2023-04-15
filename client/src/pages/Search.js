30985cf72786770d83cad60c5d6c0a007d434a5c
bjschoffman <bjschoffman@gmail.com>
2023-04-15 02:16:13 -0500

    modified search bars

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -6,6 +6,11 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Paper from "@mui/material/Paper";
+import CssBaseline from "@mui/material/CssBaseline";
 
 function Search() {
   const [lyrics, setLyrics] = useState("");
@@ -52,46 +57,71 @@ function Search() {
   return (
     <Grid container>
       <Grid item xs={12} md={6}>
-        <input
-          type="text"
-          placeholder="Song Title"
-          value={songTitle}
-          onChange={(e) => setSongTitle(e.target.value)}
-        />
-        <input
-          type="text"
-          placeholder="Artist Name"
-          value={artistName}
-          onChange={(e) => setArtistName(e.target.value)}
-        />
-        <button onClick={handleGetLyrics}>Get Lyrics</button>
-      </Grid>
-{isLoading ? renderLoading : console.log("Hello")}
-{!songDetails.title ? (
-  <p>Search Your Song</p>
-) : (
-      <Grid item xs={12} md={6}>
-      <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        component="img"
-        alt="Album cover"
-        height="140"
-        image={albumCover}
-      />
-      <CardContent>
-      <Typography variant="h1" component="h2">{songDetails.title}</Typography>
-        <Typography variant="body2" color="text.secondary">
-        {songDetails.artist}
-        </Typography>
-        <Typography><pre>{lyrics}</pre></Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Save to Library</Button>
-      </CardActions>
-    </Card>
-    </Grid>
-    )}
-        
+      <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Typography component="h1" variant="h5">
+              Search Lyrics by
+            </Typography>
+            <Box
+              component="form"
+              noValidate
+              sx={{ mt: 1 }}
+            >
+
+          <TextField
+            margin="normal"
+            fullWidth
+            label="Song Title"
+            value={songTitle}
+            onChange={(e) => setSongTitle(e.target.value)}
+            sx={{ backgroundColor: "#1f1f1f" }}
+          />
+          <TextField
+            margin="normal"
+            fullWidth
+            label="Artist Name"
+            value={artistName}
+            onChange={(e) => setArtistName(e.target.value)}
+            sx={{ backgroundColor: "#1f1f1f" }}
+          />
+          <Button onClick={handleGetLyrics}>Get Lyrics</Button>
+          </Box>
+          </Box>
+        </Grid>
+
+      {isLoading ? renderLoading : console.log("Hello")}
+      {!songDetails.title ? (
+        <p>Search Your Song</p>
+      ) : (
+            <Grid item xs={12} md={6}>
+            <Card sx={{ maxWidth: 350 }}>
+            <CardMedia
+              component="img"
+              alt="Album cover"
+              height="140"
+              image={albumCover}
+            />
+            <CardContent>
+            <Typography variant="h1" component="h2">{songDetails.title}</Typography>
+              <Typography variant="body2" color="text.secondary">
+              {songDetails.artist}
+              </Typography>
+              <Typography><pre>{lyrics}</pre></Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Save to Library</Button>
+            </CardActions>
+          </Card>
+          </Grid>
+          )}
+            
     </Grid>
   );
 }

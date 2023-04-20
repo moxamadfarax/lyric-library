efe2676ab1e6f82017bf65022e05f7d4b406e9d9
MrTofuuu <chris.stallcup@gmail.com>
2023-04-20 01:05:50 -0500

    added error handling to catchall

--- server/server.js
+++ server/server.js
@@ -31,7 +31,11 @@ app.get("/", (req, res) => {
 });
 
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  res.sendFile(path.join(__dirname, "../client/build/index.html", function(err){
+    if (err){
+      res.status(500).send(err);
+    }
+  }));
 });
 
 // app.get("/lyrics", (req, res) => {

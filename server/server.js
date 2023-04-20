77441c46075bc218c595fa563ab03e299d4c588f
MrTofuuu <chris.stallcup@gmail.com>
2023-04-20 01:03:36 -0500

    added a catchall in server js to see if it would resolve refresh issues

--- server/server.js
+++ server/server.js
@@ -30,6 +30,10 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
 // app.get("/lyrics", (req, res) => {
 //   const { artist, title } = req.query;
 //   const searchUrl = `https://api.genius.com/search?q=${encodeURIComponent(

95dd33ceff83760ec148593fd6b175e2576736c3
MrTofuuu <chris.stallcup@gmail.com>
2023-04-20 01:25:00 -0500

    removing catchall to see if removing base href in indexhtml will fix

--- server/server.js
+++ server/server.js
@@ -30,9 +30,9 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
+// app.get("/*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+// });
 
 // app.get("/lyrics", (req, res) => {
 //   const { artist, title } = req.query;
